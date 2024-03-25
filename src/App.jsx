c30792200640240f6f339b53da197365ed1c162d
Chipi87 <mgauto@outlook.es>
2024-03-25 20:57:14 -0300

    segundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
+import { db } from "./data/db";
 
 function App() {
+  const {data, setData} =useState ([db])
 
+ 
   return (
     <>
 
@@ -12,7 +16,16 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar/>
+          {data.map(() =>(
+              <Guitar/>
+            )
+            
+          )}
+
+          
+
+
+
         </div>
 
     </main>
