b25bbd702cbd69a8cd66e7801ffff2d06234d929
Marcos <mgauto@outlook.es>
2024-05-02 23:27:35 -0300

    carrito

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,11 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          {data.map(() => (
-            
-            <Guitar />
+          {data.map((guitar) => (  //siempre que iteres con un .map siempre hay que pasarle un identificador unico siempre va a tener el nombre de key
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}            //parte de la izq es el nombre del prop, es decir forma por la que vamos acceder y derecha es el valor que le vamos a pasar
+            />
 
           ))}
           
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,18 +1,27 @@
 
+// en img src se pone llaves con un template para indicarle de donde viene la imagen
+function Guitar({guitar}) {  
 
-function Guitar() {
+    const {id, name, image, description, price} = guitar // se hizo una destruction del props
+
+    const handleClick = (id) => { //le avisa que va a tomar el id
+
+    }
+
     return(
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
         <div className="col-4">
-            <img className="img-fluid" src="./public/img/guitarra_01.jpg" alt="imagen guitarra" />
+            <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />   
         </div>
         <div className="col-8">
-            <h3 className="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-            <p className="fw-black text-primary fs-3">$299</p>
+            <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+            <p>{description} </p>
+            <p className="fw-black text-primary fs-3">{price} </p>
             <button 
                 type="button"
                 className="btn btn-dark w-100"
+                onClick={ ()=> handleClick(guitar)}  //nombre de la funcion cuando el usuario ejecute el boton, en este caso llama al id para identificar que guitarra va al carrito
+                                            //si toma una funcion hay que colocar un callback para que no se llame en automatico sino que espere el evento es decir una funcion ()=>
             >Agregar al Carrito</button>
         </div>
     </div>
