888aa93f8a1838c45446e1f6506bfa11d161933b
Marcos <mgauto@outlook.es>
2024-05-06 23:19:51 -0300

    modificando carrito

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,19 @@ import { db } from "./data/db";
 
 function App() {
   const [data, setData] =useState (db)
+  const [cart, setCart] =useState([])
+
+  function addToCart (item) {   // inmutable: signifa que nos va a modificar el arreglo, es decir con el setcard esta tomando una copia del state y agregando al carrito
+                                // si lo hiciera con el .push estaria modificando el arreglo, siempre tiene que ser inmutable
+
+    const itemExists =cart.findIndex(guitar => guitar.id === item.id) // esto va a iterar sobre nuestro carrito de compras y va a crear un bojeto temporal llamado guitar
+    if(itemExists >= 0){ // aca dice que si es menor o igual a cero ya existe
+    } else{
+      item.quantity = 1
+      setCart(prevCart => [...prevCart, item]); //va a tomar el carrito previo a cambiar y sumarle
+    }
+    
+  }
 
  
   return (
@@ -20,6 +33,8 @@ function App() {
             <Guitar
               key={guitar.id}
               guitar={guitar}            //parte de la izq es el nombre del prop, es decir forma por la que vamos acceder y derecha es el valor que le vamos a pasar
+              setCart={setCart}
+              addToCart={addToCart}
             />
 
           ))}
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,11 +1,11 @@
 
 // en img src se pone llaves con un template para indicarle de donde viene la imagen
-function Guitar({guitar}) {  
+function Guitar({guitar, addToCard}) {  
 
     const {id, name, image, description, price} = guitar // se hizo una destruction del props
 
-    const handleClick = (id) => { //le avisa que va a tomar el id
-
+    const handleClick = (guitar) => { //le avisa que va a tomar el id, se reemplazo ID por guitar para avisar que tiene que tomar todos los datos
+        setCart(guitar)
     }
 
     return(
@@ -20,7 +20,7 @@ function Guitar({guitar}) {
             <button 
                 type="button"
                 className="btn btn-dark w-100"
-                onClick={ ()=> handleClick(guitar)}  //nombre de la funcion cuando el usuario ejecute el boton, en este caso llama al id para identificar que guitarra va al carrito
+                onClick={ ()=> addToCard(guitar)}  //nombre de la funcion cuando el usuario ejecute el boton,  llama al carrito setado
                                             //si toma una funcion hay que colocar un callback para que no se llame en automatico sino que espere el evento es decir una funcion ()=>
             >Agregar al Carrito</button>
         </div>
